69be9f52acf86bbed5f248892d5c41150a94efa9
theshivamlko <shivamsrivastava.9@gmail.com>
2019-09-20 01:48:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_web/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: MyHomePage(title: 'Flutter Web Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          title,
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              'https://stackwordpress.com/navoki/wp-content/uploads/2019/09/200_40.png',
+              scale: 0.5,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            Text(
+              'Welcome to Navoki',
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
